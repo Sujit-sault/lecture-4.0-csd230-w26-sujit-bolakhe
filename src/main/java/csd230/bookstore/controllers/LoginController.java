1576084efcb7222260bedd8bedb66f8431b3bd00
Sujit Bolakhe <23079797@saultcollege.ca>
2026-03-26 13:53:18 -0400

    Add Dockerfile and GitHub Actions CI pipeline

--- src/main/java/csd230/bookstore/controllers/LoginController.java
+++ src/main/java/csd230/bookstore/controllers/LoginController.java
@@ -3,8 +3,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
-    @GetMapping("/login")
+    //    @GetMapping("/login")
     public String login() {
         return "login";
     }
 }
+
