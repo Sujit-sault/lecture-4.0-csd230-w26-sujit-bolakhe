c88eb36f6ffcd2fa25a7eddbf747b316f32919b8
Sujit Bolakhe <23079797@saultcollege.ca>
2026-03-26 14:50:34 -0400

    Update HomePage

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div style={{ textAlign: 'center', padding: '50px' }}>
-            <h1>Welcome to Sujit's Bookstore Admin</h1>
+            <h1>Sujit's Bookstore</h1>
             <p>Use the navigation bar above to manage your digital library.</p>
             <div style={{ fontSize: '50px' }}>📖🚀</div>
         </div>
